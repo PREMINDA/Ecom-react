2626d40f699be3dc3a18578f5712c46849d47ab4
PREMINDA <premindabandara0777@gmail.com>
2021-05-13 14:40:06 +0530

    map Collection item to Commecion component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,10 +8,15 @@ import { selectCollection } from "./../../redux/collection/shop.selector";
 import "./collection.style.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>page cat</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
